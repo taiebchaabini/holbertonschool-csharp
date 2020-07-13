8f87d084819ea9486e62b97da342bc43f136f230
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-13 15:17:59 +0000

    Fixing script

--- 0x00-csharp-hello_world/2-run_new_project.sh
+++ 0x00-csharp-hello_world/2-run_new_project.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Bash script that initializes, build and run a new C# project 
-dotnet new console -o 2-new_project. && cd 2-new_project && dotnet build && dotnet run
+dotnet new console -o 2-new_project. && cd 2-new_project && dotnet build && dotnet run && cd ..
