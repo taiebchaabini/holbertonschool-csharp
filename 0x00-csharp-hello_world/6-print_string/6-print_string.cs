8c2867348861225912e9d39f9747f3a8da978fe7
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-13 17:49:50 +0000

    First commit

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "Holberton School";
+        string str2 = $"{str}{str}{str}\n";
+        Console.Write(str2);
+        Console.WriteLine(str.Substring(0, 9));
+    }
+}
