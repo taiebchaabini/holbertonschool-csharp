2add14af82a6a2bf71a8dab00e3ae77026972dac
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-13 15:36:17 +0000

    Update 2-run_new_project.sh

--- 0x00-csharp-hello_world/2-run_new_project.sh
+++ 0x00-csharp-hello_world/2-run_new_project.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 # Bash script that initializes, build and run a new C# project 
-<<<<<<< HEAD
 dotnet new console -o 2-new_project
 dotnet build 2-new_project 
 dotnet run --project 2-new_project/2-new_project.csproj
-=======
-dotnet new console -o 2-new_project && cd 2-new_project && dotnet build && dotnet run && cd ..
->>>>>>> 9037856c1e59e27871c166f24f0ba22dfe618d85
