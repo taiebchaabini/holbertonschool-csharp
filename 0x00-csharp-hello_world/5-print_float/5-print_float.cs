a59f15b51056a98f863caa124242e8a5df9aa590
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-13 17:41:32 +0000

    Fixing spaces

--- 0x00-csharp-hello_world/5-print_float/5-print_float.cs
+++ 0x00-csharp-hello_world/5-print_float/5-print_float.cs
@@ -4,7 +4,7 @@ class Program
 {
 	static void Main(string[] args)
         {
-		float number = 3.14159f;
-        Console.WriteLine("Float: {0}", number.ToString("#.##"));
+            float number = 3.14159f;
+            Console.WriteLine("Float: {0}", number.ToString("#.##"));
         }
 }
