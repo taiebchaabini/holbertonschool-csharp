c0a09ce1ffd79647df21505fa8627b9e02760c35
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-14 15:44:43 +0000

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 9; i++){
+                for (int b = i + 1; b < 10; b++){
+                    if (i == 8 && b == 9)
+                        Console.Write($"{i}{b}\n");
+                    else
+                        Console.Write($"{i}{b}, ");
+                }
+            }
+        }
+    }
+}
