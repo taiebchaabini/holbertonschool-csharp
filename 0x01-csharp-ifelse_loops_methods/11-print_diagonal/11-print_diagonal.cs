6d24a936b9d0d5a248eb1c0d1adf0615bda6cddc
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-14 16:52:52 +0000

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace _11_print_diagonal
+{
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            var output = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                output.Clear();
+                output.Insert(0, " ", i);
+                Console.Write($"{output}\\\n");
+            }
+            Console.Write("\n");
+        }
+    }
+}
