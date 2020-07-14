73b83fd54790bfcd743b17430470e836317aed73
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-14 15:17:29 +0000

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 99; i++){
+                Console.Write("{0:d2}", i);
+                if (i != 99)
+                    Console.Write(", ");
+            }
+        }
+    }
+}
