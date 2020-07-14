efd911d5d3ca0aaf68fbab6ff726b20343229850
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-14 14:53:30 +0000

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 97; i <= 122; i++)
+                Console.Write("{0}", Convert.ToChar(i));
+        }
+    }
+}
