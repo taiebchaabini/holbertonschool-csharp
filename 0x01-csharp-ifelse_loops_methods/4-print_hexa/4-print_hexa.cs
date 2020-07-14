75ca573dabe2932b5b1aa35712654781396dc65a
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-14 15:11:33 +0000

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 98; i++){
+                Console.Write($"{i} = 0x{i:x}\n");
+            }
+        }
+    }
+}
