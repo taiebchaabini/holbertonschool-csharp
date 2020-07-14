79a620af56b06b8f7548a34b4de767c71e6b6730
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-14 15:19:56 +0000

    Fixing new line

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -7,9 +7,10 @@ namespace _5_print_comb
         static void Main(string[] args)
         {
             for(int i = 0; i <= 99; i++){
-                Console.Write("{0:d2}", i);
                 if (i != 99)
-                    Console.Write(", ");
+                    Console.Write("{0:d2}, ", i);
+                else
+                    Console.Write("{0:d2}\n", i);
             }
         }
     }
