4299eaed5254bb9aa1d543cf8a677d3ab265578d
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-14 16:15:08 +0000

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace _10_print_line
+{
+    class Line
+    {
+        public static void PrintLine(int length){
+            var output = new StringBuilder();
+            for (int i = 0; i < length; i++){
+                output.Append("_");
+            }
+            Console.Write($"{output}\n");
+        }
+    }
+}
