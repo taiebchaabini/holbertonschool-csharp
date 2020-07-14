58bc10f894e88951535cd5b2e126efc05a42f20e
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-14 17:02:31 +0000

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var output = new StringBuilder();
+            for (int i = 1; i <= 100; i++){
+                if (i % 3 == 0 && i % 5 == 0){
+                    output.Append("FizzBuzz");
+                }
+                else if (i % 3 == 0){
+                    output.Append("Fizz");
+                }
+                else if (i % 5 == 0){
+                    output.Append("Buzz");
+                }
+                else{
+                    output.Append($"{i}");
+                }
+                if (i != 100){
+                    output.Append(" ");
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
