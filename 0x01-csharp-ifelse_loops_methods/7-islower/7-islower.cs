a6dc925314afb4598a2d425272d3ae19416cd60c
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-14 15:57:01 +0000

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _7_islower
+{
+    class Character{
+        public static bool IsLower(char c){
+            int ascii = System.Convert.ToInt32(c);
+            if (ascii >= 97 && ascii <= 122)
+                return true;
+            return false;
+        }
+    }
+}
