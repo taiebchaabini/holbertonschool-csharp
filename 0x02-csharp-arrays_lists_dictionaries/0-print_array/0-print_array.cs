92e8e9e08ac183304e2eb3c2caf56b5114129b0d
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-16 16:16:48 +0000

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace _0_print_array
+{
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+
+            var arr = new int[size];
+            var output = new StringBuilder();
+
+            for (int i = 0; i < size; i++)
+            {
+                output.Append(i);
+                if (i != size - 1)
+                {
+                    output.Append(" ");
+                }
+                arr[i] = i;
+            }
+
+            Console.WriteLine(output);
+
+            return arr;
+        }
+    }
+}
