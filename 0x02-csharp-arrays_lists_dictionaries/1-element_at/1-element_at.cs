f2899ff42a4fbfdb9602f6f93b08d0637eef3fcb
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-16 17:42:09 +0000

    Fixing length

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -3,7 +3,7 @@
 class Array
 {
     public static int elementAt(int[] array, int index){
-        if (index < 0 || index > array.Length){
+        if (index < 0 || index > array.Length - 1){
             Console.WriteLine("Index out of range");
             return -1;
         }
