80340c2fee1f08680f21722b0a5150e93cc4a58f
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-16 19:10:49 +0000

    Back to string builder

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -11,18 +11,18 @@ class Array
         }
 
         int[] arr = new int[size];
-
+        var output = new StringBuilder();
         for (int i = 0; i < size; i++)
         {
-            Console.Write(i);
+            output.append(i);
             if (i != size - 1)
             {
-                Console.Write(" ");
+                output.Append(" ");
             }
             arr[i] = i;
         }
 
-        Console.Write("\n");
+        Console.WriteLine(output);
 
         return arr;
     }
