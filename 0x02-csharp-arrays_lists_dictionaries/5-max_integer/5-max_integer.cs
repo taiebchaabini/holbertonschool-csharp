ca09beeed6edb23355585326af4e59a521e36217
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-16 19:05:46 +0000

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList){
+        if (myList.Count == 0){
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+        int max = 0;
+        foreach (int element in myList){
+            if (element > max){
+                max = element;
+            }
+        }
+        return max;
+    }
+}
