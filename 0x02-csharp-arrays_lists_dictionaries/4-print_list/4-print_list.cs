92b5618d5016565b7e12cba2a75aaed40fc6ef0e
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-16 18:39:02 +0000

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> CreatePrint(int size){
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> myList = new List<int>();
+        var output = new StringBuilder();
+        for (int i = 0; i < size; i++)
+        {
+            myList.Add(i);
+            output.Append(i);
+            if (i != size)
+                output.Append(" ");
+        }
+        Console.WriteLine(output);
+        return myList;
+    }
+}
