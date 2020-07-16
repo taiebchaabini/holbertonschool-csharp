04b70dfd698046327382b2d448cf29aef87df0a5
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-16 16:36:02 +0000

    Cleaning file

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,48 +1,29 @@
 ﻿using System;
 
-namespace _0_print_array
+class Array
 {
-    class Array
+    public static int[] CreatePrint(int size)
     {
-        static void Main(string[] args)
-    {
-        int[] newArray;
-
-        newArray = Array.CreatePrint(10);
-        Console.WriteLine("Array Length:" + newArray.Length);
-        Console.WriteLine("----------------");
-        newArray = Array.CreatePrint(16);
-        Console.WriteLine("Array Length:" + newArray.Length);
-        Console.WriteLine("----------------");
-        newArray = Array.CreatePrint(0);
-        Console.WriteLine("Array Length:" + newArray.Length);
-        Console.WriteLine("----------------");
-        newArray = Array.CreatePrint(-10);
-    }
-        public static int[] CreatePrint(int size)
+        if (size < 0)
         {
-
-            if (size < 0)
-            {
-                Console.WriteLine("Size cannot be negative");
-                return null;
-            }
-            
-            int[] arr = new int[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                Console.Write(i);
-                if (i != size - 1)
-                {
-                    Console.Write(" ");
-                }
-                arr[i] = i;
-            }
-
-            Console.Write("\n");
-
-            return arr;
+            Console.WriteLine("Size cannot be negative");
+            return null;
         }
+
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(i);
+            if (i != size - 1)
+            {
+                Console.Write(" ");
+            }
+            arr[i] = i;
+        }
+
+        Console.Write("\n");
+
+        return arr;
     }
 }
