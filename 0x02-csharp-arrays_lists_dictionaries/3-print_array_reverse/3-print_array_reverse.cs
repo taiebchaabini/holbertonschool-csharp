59d3e37b8b936818812adb9b8f5596203bf541f9
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-16 18:13:27 +0000

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        var output = new StringBuilder();
+        if (array != null)
+        {
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                output.Append(array[i]);
+                if (i != 0)
+                    output.Append(" ");
+            }
+        }
+        Console.WriteLine(output);
+    }
+}
