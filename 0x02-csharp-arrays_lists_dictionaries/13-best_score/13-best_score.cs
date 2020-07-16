02769a96869535697c5813e73ca1e19b013db618
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-16 20:51:10 +0000

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList){
+        int max = 0;
+        string maxKey = "";
+        foreach(var pair in myList){
+            if (pair.Value > max){
+                max = pair.Value;
+                maxKey = pair.Key; 
+            }
+        }
+        return maxKey;
+    }
+}
