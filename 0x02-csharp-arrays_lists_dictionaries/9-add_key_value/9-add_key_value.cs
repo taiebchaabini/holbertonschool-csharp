4be23ca2fe099c7844ef6024367dadef118c126e
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-16 19:45:06 +0000

    Fix to method

--- 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 class Dictionary
 {
     public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value){
-        myDict.Add(key, value);
+        myDict[key] = value;
         return myDict;
     }
 }
