ce63c17aadfe560127acbf9572768864fb92be02
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-27 13:59:59 +0100

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2){
+        List<int> different = new List<int>();
+        foreach (var elem in list1){
+            if (!list2.Contains(elem)){
+                different.Add(elem);
+            }
+        }
+        foreach (var elem in list2){
+            if (!list1.Contains(elem)){
+                different.Add(elem);
+            }
+        }
+        different.Sort();
+        return different;
+    }
+}
