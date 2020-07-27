b72b44a9c5270385197aa1c0900f1b312a99fe08
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-27 18:32:20 +0100

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size){
+        LinkedList<int> myList = new LinkedList<int>();
+        
+        for (int i = 0; i < size; i++){
+            myList.AddLast(i);
+            Console.WriteLine(i);
+        }
+        return myList;
+    }
+}
