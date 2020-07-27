0f5671a922bf583056ccd4b4111ac636ae4c1c5c
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-27 20:02:00 +0100

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList){
+        if (myLList.Count == 0)
+            return 0;
+        LinkedListNode<int> head = myLList.First;
+        return head.Value;
+    }
+}
