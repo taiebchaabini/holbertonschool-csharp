e394d2179fa1bc42bf302f715aeb427e0616eb14
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-27 17:33:40 +0100

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search){
+        string topItem;
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if (aQueue.Count == 0){
+            topItem = "Queue is empty";
+        } else{
+            topItem = "First item: " + aQueue.Peek();
+        }
+        Console.WriteLine(topItem);
+        aQueue.Enqueue(newItem);
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+        while(aQueue.Contains(search)){
+            aQueue.Dequeue();
+        }
+        return aQueue;
+    }
+}
