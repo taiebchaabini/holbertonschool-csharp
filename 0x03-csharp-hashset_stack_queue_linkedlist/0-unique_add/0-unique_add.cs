7c5ed18aaf6c933f1fe1d265cfd98a83e8f6b4ca
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-27 13:39:06 +0100

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList){
+        List<int> newList = new List<int>();
+        int sum = 0;
+        foreach (var elem in myList){
+            if (! newList.Contains(elem)){
+                newList.Add(elem);
+                sum += elem;
+            }
+        }
+        return sum;
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/README.md
@@ -0,0 +1,10 @@
+# 0x03-csharp-hashset_stack_queue_linkedlist
+- General
+- How to create and use arrays
+- What are Collections in C#?
+- How to create and use hashsets
+- How to create and use stacks
+- How to create and use queues
+- How to create and use linked lists
+- How to use foreach
+- Requirements
