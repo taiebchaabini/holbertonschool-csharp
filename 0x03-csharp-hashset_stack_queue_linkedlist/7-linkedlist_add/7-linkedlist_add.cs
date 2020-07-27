1ee8dfd513470ff99b14aa3f3bc0ef52d53d3d9a
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-27 19:21:26 +0100

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n){
+        LinkedListNode<int> node = myLList.AddFirst(n);
+        return node;
+    }
+}
