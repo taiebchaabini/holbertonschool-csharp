b24f66a9d188391dce96826bb5df1871931d56b8
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-27 16:16:40 +0100

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search){
+        var topItem = "";
+
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        if (aStack.Count == 0){
+            topItem = "Stack is empty";
+        } else{
+            topItem = "Top item: " + aStack.Peek();
+        }
+        Console.WriteLine(topItem);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
+        while(aStack.Contains(search)){
+            aStack.Pop();
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
