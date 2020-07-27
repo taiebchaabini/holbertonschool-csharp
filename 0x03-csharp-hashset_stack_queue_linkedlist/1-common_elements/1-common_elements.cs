3671117e89b9818cfd47294481be217ad5a33832
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-27 13:49:53 +0100

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2){
+        List<int> list3 = new List<int>();
+
+        foreach (var elem in list1){
+            if (list2.Contains(elem)){
+                list3.Add(elem);
+            }
+        }
+        return list3;
+    }
+}
