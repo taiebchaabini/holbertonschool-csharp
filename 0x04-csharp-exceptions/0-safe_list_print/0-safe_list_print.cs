767aaa1a6a27723d370b75fa02acac71aa24ffb3
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-28 15:52:45 +0100

    First commit

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int i = 0;
+        try
+        {
+            while(i < n){
+                Console.WriteLine(myList[i]);
+                i++;
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+        return i;
+    }
+}
---
+++ 0x04-csharp-exceptions/README.md
@@ -0,0 +1,8 @@
+# 0x04. C# - Exceptions
+- General
+- What are exceptions and when to use them
+- What is the purpose of catching exceptions
+- How to handle exceptions
+- How to use try, catch, finally
+- How to raise built-in exceptions
+- When to implement clean-up actions after exceptions
