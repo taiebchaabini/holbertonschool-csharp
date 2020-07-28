f638809d0c89c5dfd06d492ee3f178a34a4e94a9
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-28 15:57:48 +0100

    First commit

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b){
+        int res = 0;
+        try{
+            res = a / b;
+        }
+        catch(DivideByZeroException){
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally{
+            Console.WriteLine("{0} / {1} = {2}", a, b, res);
+        }
+    }
+    
+}
