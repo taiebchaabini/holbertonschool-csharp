cede79658b65e484cfd25b9808248989e732ba8b
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-28 16:57:45 +0100

    First commit

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public enum Rating{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog{
+    public Dog(string dogName, float dogAge, string dogOwner, Rating dogRating){
+        name = dogName;
+        age = dogAge;
+        owner = dogOwner;
+        rating = dogRating;
+    }
+    public string name { get; }
+    public float age { get; }
+    public string owner { get; }
+    public Rating rating { get; }
+    public override string ToString() => $"Dog Name: {name}\nAge: {age}\nOwner: {owner}\nRating: {rating}";
+}
