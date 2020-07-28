b121a603102d8acb70ec4dddc6c650022dab08f1
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-28 16:45:30 +0100

    First commit

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public enum Rating{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog{
+    public Dog(string dogName, float dogAge, string dogOwner, Rating dogRating){
+        name = dogName;
+        age = dogAge;
+        owner = dogOwner;
+        rating = dogRating;
+    }
+    public string name { get; }
+    public float age { get; }
+    public string owner { get; }
+    public Rating rating { get; }
+}
