18b2aec8b3960712cd7175cd4bd35ae5c10e6842
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-30 18:40:12 +0100

    Fixes

--- 0x06-csharp-classes/4-enemy/4-enemy.cs
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -12,21 +12,35 @@ namespace Enemies
         // Private Field that defines the name of a zombie.
         private string name = "(No name)";
         /// <summary>Gets or sets the name of a zombie.</summary>
-        public string Name { get; set; }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
         /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
-        public Zombie(){
+        public Zombie()
+        {
             health = 0;
         }
         /// <summary>Initializes a new instance of the <see cref="Zombie"/> class and checks if value is greater than 0.</summary>
-        public Zombie(int value){
-            if (value < 0){
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
                 throw new ArgumentException("Health must be greater than or equal to 0");
             }
             health = value;
         }
 
         /// <summary>Gets the health of a zombie.</summary>
-        public int GetHealth(){
+        public int GetHealth()
+        {
             return health;
         }
 
