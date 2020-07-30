47f7d4dec75f4c775131596436ba968e35aa706a
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-30 18:47:23 +0100

    First commit

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This class creates a zombie.
+    /// </summary>
+    public class Zombie
+    {
+        // Private Field that defines the health of azombie.
+        private int health;
+        // Private Field that defines the name of a zombie.
+        private string name = "(No name)";
+        /// <summary>Gets or sets the name of a zombie.</summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class and checks if value is greater than 0.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        /// <summary>Gets the health of a zombie.</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+        /// <summary> override that prints the Zombie object’s attributes to stdout, Format: Zombie name: <name> / Total Health: <health> </summary>
+        public override string ToString(){
+            return($"Zombie name: {Name} / Total Health: {GetHealth()}");
+        }
+    }
+}
