bb7574b851a56c7002b301c6775998656dc89513
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-04 23:53:46 +0100

    First commit

---
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+using Text;
+
+namespace Text.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [Test]
+        public void TestNull()
+        {
+            Assert.AreEqual(0, Text.Str.CamelCase(null));
+        }
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, Text.Str.CamelCase("Hello"));
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(2, Text.Str.CamelCase("Hello World"));
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(3, Text.Str.CamelCase("Hello my Friend Hello"));
+        }
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(1, Text.Str.CamelCase("Hello"));
+        }
+        [Test]
+        public void Test5(){
+            Assert.AreEqual(0, Text.Str.CamelCase("123"));
+        }
+        [Test]
+        public void Test6(){
+            Assert.AreEqual(1, Text.Str.CamelCase("123 Hi"));
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Text
+{
+     /// <summary>Represents a Str class.</summary>
+    public class Str
+    {
+        /// <summary>Checks the index of the first non-repeating character of a string</summary>
+        /// <param name="s">String to check.</param>
+        /// <returns>returns the index of the first non-repeating character of a string.</returns>
+
+        public static int CamelCase(string s){
+            if (s == null){
+                return 0;
+            }
+            string[] list = s.Split(' ');
+            int i = 0;
+            int n = 0;
+            if (list.Length == 1 && !Char.IsLetter(s[0]))
+                return 0;
+            if (list.Length == 1){
+                return 1;
+            }
+            if (list.Length > 1 && Char.IsLetter(s[0])){
+                n += 1;
+            }
+            foreach (var str in list){
+                if (Char.IsLetter(str[0]) && i > 0 && Char.IsUpper(str[0]))
+                    n += 1;
+                i += 1;
+            }
+            return n;
+        }
+    }
+}
