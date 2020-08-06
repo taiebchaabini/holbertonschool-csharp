9700863eba62e14e1bca597c236320f60a0b38e7
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-06 18:12:16 +0100

    First commit

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    public static bool IsInstanceOfArray(object obj){
+        if (obj.GetType() == typeof(Array) || obj is Array)
+            return true;
+        return false;
+    }
+}
