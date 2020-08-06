7c1c7319e12c9642a69b8d53e59d8b0a7855992e
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-06 18:06:34 +0100

    First commit

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    public static bool IsOfTypeInt(object obj){
+        if (obj.GetType() == typeof(int)){
+            return true;
+        }
+        return false;
+    }
+}
