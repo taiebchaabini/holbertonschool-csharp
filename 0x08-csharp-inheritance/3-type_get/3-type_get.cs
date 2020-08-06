c1645d78f071bc2aa116cd0e8dec5863dbfbb4fc
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-06 19:15:09 +0100

    First commit

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+/// <summary>Represents the class Obj.</summary>
+class Obj
+{
+    /// <summary>
+    /// prints the names of the available properties and methods of an object.
+    /// </summary>
+    /// <param name="myObj"></param>
+    public static void Print(object myObj){
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        IEnumerable<PropertyInfo> pList = t.DeclaredProperties;
+        IEnumerable<MethodInfo> mList = t.DeclaredMethods;
+        Console.WriteLine("{0} Properties:", myObj.GetType().Name);
+        foreach (var p in pList)
+        {
+            Console.Write(p.IsSpecialName + ": ");
+            Console.WriteLine(p.Name);
+        }
+        Console.WriteLine("{0} Methods:", myObj.GetType().Name);
+        foreach (var m in mList)
+        {
+            Console.WriteLine(m.Name);
+        }
+
+    }
+}
