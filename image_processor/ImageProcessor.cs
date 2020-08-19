e7d6346d5f4de9f117cad346ac0ad1679962de95
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-19 23:51:46 +0100

    Task 1

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -19,7 +19,7 @@ class ImageProcessor
 
         Parallel.ForEach(filenames, (myFile) =>
         {
-            
+
             Bitmap image = new Bitmap(myFile);
             var extension = Path.GetExtension(myFile);
             var fileName = Path.GetFileNameWithoutExtension(myFile);
@@ -38,7 +38,53 @@ class ImageProcessor
 
             for (int counter = 0; counter < rgbValues.Length; counter += 1)
             {
-                rgbValues[counter] = (byte)~rgbValues[counter];
+                rgbValues[counter] = (byte)(255 - rgbValues[counter]);
+            }
+
+
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, pointer, bitmapBytes);
+            image.UnlockBits(imageData);
+
+            image.Save($"{fileName}");
+        });
+    }
+    /// <summary>
+    /// Method that grayscale an image
+    /// </summary>
+    /// <param name="filenames">files to operate for grayscale</param>
+    public static void Grayscale(string[] filenames)
+    {
+
+        Parallel.ForEach(filenames, (myFile) =>
+        {
+
+            Bitmap image = new Bitmap(myFile);
+            var extension = Path.GetExtension(myFile);
+            var fileName = Path.GetFileNameWithoutExtension(myFile);
+            fileName += "_grayscale" + extension;
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+            System.Drawing.Imaging.BitmapData imageData =
+            image.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+            image.PixelFormat);
+
+            IntPtr pointer = imageData.Scan0;
+
+            var bitmapBytes = Math.Abs(imageData.Stride) * image.Height;
+            var rgbValues = new byte[bitmapBytes];
+
+            System.Runtime.InteropServices.Marshal.Copy(pointer, rgbValues, 0, bitmapBytes);
+
+            for (int counter = 0; counter < rgbValues.Length; counter += 3)
+            {
+                var gray = (rgbValues[counter] + rgbValues[counter + 1] + rgbValues[counter + 2]) / 3;
+                rgbValues[counter] = (byte)gray;
+                if (counter + 1 < rgbValues.Length){
+                    rgbValues[counter + 1] = (byte)gray;
+                }
+                if (counter + 2 < rgbValues.Length){
+                    rgbValues[counter + 2] = (byte)gray;
+                }
+
             }
 
 
