88667e6452ce3581656eb3186e6a30d374b25d6e
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-25 23:28:12 +0100

    Changing the round to nearest tenth

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -8,7 +8,7 @@ class VectorMath
     /// Method that calculates and returns the length of a given vector.
     /// </summary>
     /// <param name="vector">2D or 3D vector.</param>
-    /// <returns>returns the length rounded to the nearest hundredth.</returns>
+    /// <returns>returns the length rounded to the nearest tenth.</returns>
     public static double Magnitude(double[] vector){
         if (vector.Length != 3 && vector.Length != 2){
             return (-1);
@@ -17,6 +17,6 @@ class VectorMath
         foreach (var elem in vector){
             sum += elem * elem;
         }
-        return System.Math.Round(System.Math.Sqrt(sum), 2);
+        return System.Math.Round(System.Math.Sqrt(sum), 1);
     }
 }
