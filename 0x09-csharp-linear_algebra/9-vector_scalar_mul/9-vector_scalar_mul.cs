0c855851f9f9b132922eba40dee0ad8cdfd61a34
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-25 23:50:45 +0100

    First commit

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// VectorMath main class.
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Method that multiplies a vector and a scalar and returns the resulting vector.
+    /// </summary>
+    /// <param name="vector">Vector to multiply</param>
+    /// <param name="scalar">Number of multiplication</param>
+    /// <returns>returns the resulting vector after multiplication.</returns>
+    public static double[] Multiply(double[] vector, double scalar){
+        if (vector.Length != vector.Length){
+                return (new Double[1]{-1});
+        }
+        var sum = new Double[3];
+        for (int i = 0; i < vector.Length; i++){
+            sum[i] = vector[i] * scalar;
+        }
+        return sum;
+    }
+}
