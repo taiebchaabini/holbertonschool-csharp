77fb7a99594f5cf9836fd8e7de3c1ea8ec9e3522
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-26 15:55:36 +0100

    Fixed

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -12,7 +12,9 @@ class MatrixMath
     /// <param name="matrix2">Second matrix (2D or 3D)</param>
     /// <returns>returns the resulting matrix.</returns>
     public static double[,] Add(double[,] matrix1, double[,] matrix2){
-        if (matrix1.Length != matrix2.Length ||
+        
+        if (matrix1.GetLength(0) != matrix2.GetLength(0) ||
+            matrix1.GetLength(1) != matrix2.GetLength(1) ||
             matrix1.GetLength(0) == 2 && matrix1.GetLength(1) != 2 || 
             matrix1.GetLength(0) == 3 && matrix1.GetLength(1) != 3 ||
             matrix2.GetLength(0) == 2 && matrix2.GetLength(1) != 2 || 
