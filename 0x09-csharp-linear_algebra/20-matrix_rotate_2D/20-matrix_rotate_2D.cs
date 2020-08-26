e0720c7936edf095787818bce57b7ea471910d7d
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-26 22:32:22 +0100

    Fixed

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -37,9 +37,13 @@ class MatrixMath
     /// <param name="angle">New angle</param>
     /// <returns>returns the resulting matrix.</returns>
     public static double[,] Rotate2D(double[,] matrix, double angle){
+        if (matrix.Lengthh <= 1 || 
+            matrix.GetLength[0] != matrix.GetLength[1]){
+            return (new Double[,]{{-1}});
+        }
         double radians = (System.Math.PI / 180) * angle;
-        double cosAngle = System.Math.Cos(radians);
-        double sinAngle = System.Math.Sin(radians);
+        double cosAngle = System.Math.Cos(angle);
+        double sinAngle = System.Math.Sin(angle);
         var rotation = new Double[2,2]{{cosAngle, -sinAngle}, {sinAngle, cosAngle}};
         var res = new Double[matrix.GetLength(0),matrix.GetLength(1)];
         res = Multiply(matrix, rotation);
