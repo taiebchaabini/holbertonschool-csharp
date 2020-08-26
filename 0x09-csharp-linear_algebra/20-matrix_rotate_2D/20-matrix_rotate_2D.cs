920ff5258fefd6ae96aac4c70b78c153b269b055
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-26 22:12:43 +0100

    Fixed

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -6,26 +6,6 @@
 class MatrixMath
 {
     /// <summary>
-    /// Method that multiplies two matrices and returns the resulting matrix.
-    /// </summary>
-    /// <param name="matrix1">Matrix 1</param>
-    /// <param name="matrix2">Matrix 2</param>
-    /// <returns>returns the new matrix</returns>
-    public static double[,] Multiply(double[,] matrix1, double[,] matrix2){
-        var res = new double[matrix1.GetLength(0),matrix2.GetLength(1)];
-        double sum = 0.0;
-        for (int y = 0; y < matrix1.GetLength(0); y++){
-            for (int x2 = 0; x2 < matrix2.GetLength(1); x2++){
-                sum = 0;
-                for (int x = 0; x < matrix1.GetLength(1); x++){
-                    sum += matrix1[y,x] * matrix2[x, x2];
-                }
-                res[y, x2] = sum;
-            }
-        }
-        return res;  
-    }
-    /// <summary>
     /// Method that rotates a square 2D matrix by a given angle in radians
     /// </summary>
     /// <param name="matrix">Matrix to rotate</param>
@@ -40,8 +20,17 @@ class MatrixMath
         double cosAngle = System.Math.Cos(angle);
         double sinAngle = System.Math.Sin(angle);
         var rotation = new Double[2,2]{{cosAngle, -sinAngle}, {sinAngle, cosAngle}};
-        var res = new Double[2,2];
-        res = Multiply(matrix, rotation);
+        var res = new double[matrix.GetLength(0),matrix.GetLength(1)];
+        double sum = 0.0;
+        for (int y = 0; y < matrix.GetLength(0); y++){
+            for (int x2 = 0; x2 < rotation.GetLength(1); x2++){
+                sum = 0;
+                for (int x = 0; x < matrix.GetLength(1); x++){
+                    sum += matrix[y,x] * rotation[x, x2];
+                }
+                res[y, x2] = sum;
+            }
+        }
         return res;
     }
 }
