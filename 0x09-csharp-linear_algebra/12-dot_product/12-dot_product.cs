ede5077f8a45138ef6cf8b5c5da110e6129619ae
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-26 15:17:01 +0100

    Fix to task

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -15,7 +15,7 @@ class VectorMath
         if (vector1.Length != vector2.Length ||
             vector1.Length != 3 && vector1.Length != 2 || 
             vector2.Length != 3 && vector2.Length != 2){
-                return (new Double[1]{-1});
+                return (-1);
         }
         double sum = 0;
         for (int x = 0; x < vector1.Length; x++){
