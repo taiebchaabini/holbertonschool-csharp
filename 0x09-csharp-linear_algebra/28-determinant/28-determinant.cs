24f336aa75dd2efbf8bbeca6e48563f5ff46d242
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-28 23:12:24 +0100

    Fixed

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -16,7 +16,7 @@ class MatrixMath
     /// <param name="matrix">2D or 3D matrix.</param>
     /// <returns>returns the determinant of a matrix</returns>
     public static double Determinant(double[,] matrix){
-        if (matrix.GetLength >= 3 || 
+        if (matrix.GetLength(1) > 3 || 
             matrix.GetLength(1) != matrix.GetLength(0)){
             return (-1);
         }
