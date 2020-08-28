c8d46ea93156140a2b9c062f2071f664bf2b64ae
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-28 23:04:56 +0100

    Fixed

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -23,9 +23,9 @@ class MatrixMath
             return (Determinant2D(matrix));
         }
         var matrixA = new Double[,]{{matrix[1,1], matrix[1,2]},{matrix[2,1], matrix[2,2]}};
-        var matrixB = new Double[,]{{matrix[2,0], matrix[2,2]},{matrix[3,0], matrix[3,2]}};
-        var matrixC = new Double[,]{{matrix[2,0], matrix[2,1]},{matrix[3,0], matrix[3,1]}};
-        var res = Determinant2D(matrixA) - Determinant2D(matrixB) + Determinant2D(matrixC);
+        var matrixB = new Double[,]{{matrix[1,0], matrix[1,2]},{matrix[2,0], matrix[2,2]}};
+        var matrixC = new Double[,]{{matrix[1,0], matrix[1,1]},{matrix[2,0], matrix[2,1]}};
+        var res = ((matrix[0,0] * Determinant2D(matrixA)) - (matrix[0,1] * Determinant2D(matrixB))) + matrix[0,2] * Determinant2D(matrixC);
         return res;
     }
 }
