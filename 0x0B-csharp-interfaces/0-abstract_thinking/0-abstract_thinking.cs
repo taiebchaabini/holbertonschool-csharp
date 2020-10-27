8bf3f87302bbb0dc6a84727a68dc9be2b07a84a0
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-10-27 13:40:16 +0100

    First commit

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Base class
+/// </summary>
+abstract class Base{
+    /// <summary>
+    /// Name property
+    /// </summary>
+    public String name;
+    /// <summary>
+    /// Overrides ToString Method
+    /// </summary>
+    public override string ToString(){
+        return String.Format("{0} is a {1}", name, this.GetType());
+    }
+}
